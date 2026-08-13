b985f82e1671707b2c9a801c03f036357307602f
subhendhukumarmohapatra-stack <subhendhukumarmohapatra@gmail.com>
2026-08-13 10:24:23 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,107 @@
+
+// alert("Welcome to Amazon.in")
+
+// console.log("Hellow")
+
+// let a = Number(prompt("1st Num : "));
+// let b = Number(prompt("2nd Num : "));
+// c = a + b;
+// console.log(c)
+
+
+
+// let gameNum = 2006;
+
+// userNum = prompt("Unlock to access your website :");
+// while(gameNum != userNum) {
+//     userNum = prompt("Wrong ! Please try again");
+// }
+// console.log("Congratulation You geuss Right Number");
+
+
+let UserId = "tapu_7";
+let PassWord = 2006;
+
+userInput = prompt("User Id :");
+while(UserId != userInput) {
+    userInput = prompt("Wrong User Id !");
+}
+userPass = prompt("Password :");
+while(PassWord != userPass) {
+    userPass = prompt("Wrong Password !");
+}
+alert("Login Succcessfully")
+
+
+
+// let userEmail = prompt("Enter Your Email id :");
+
+// userEntered = (userEmail.toLowerCase() + "@gmail.com");
+
+// console.log(userEntered.replaceAll(" ", ""));
+
+
+
+
+// let fullName = prompt("Enter Your Full Name: ");
+
+// let userName = "@" + fullName.toLocaleLowerCase() + fullName.length;
+
+// console.log(userName.replaceAll(" ", ""));
+
+
+// let marks = [40, 50, 80, 70, 40, 20];
+
+// let sum = 0;
+
+// for(let valu of marks) {
+//     sum += valu;
+// }
+
+// let avg = sum / marks.length;
+
+// console.log("AVRAGE MARKS OF THE CLASS: ", avg );
+// console.log(`Avrage marks of the class: ${avg}`);
+
+
+// let Item = [250, 645, 300, 900, 50];
+
+// let i = 0;
+
+// for(let val of Item) {
+//     let offer = val / 10;
+//     Item[i] = Item[i] - offer;
+//     console.log(`value after offer = ${Item[i]}`);
+//     i++;
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 5) {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     if (i === 3) {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+// let name = "SuvEndu";
+
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+// let text = "JavaScript Programming";
+
+// console.log(text.charAt(5));
+// console.log(text.includes("Script"));
+// console.log(text.indexOf("Programming"));
+// console.log(text.slice(0, 10));
+// console.log(text.replace("JavaScript", "Python"));
+
